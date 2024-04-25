424cf0079d4f8b0d85f6211a495c6be8f7f7a2b5
sebsibe23 <sebsibe1990@proton.me>
2024-04-25 09:40:26 -0700

    final

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,42 +1,21 @@
 #!/usr/bin/python3
-'''Module for working with Pascal's triangle.
-
-This module provides functionality to generate Pascal's
-triangle up to a given integer.
+'''th module for working with Pascal's triangle.
 '''
 
 
 def pascal_triangle(n):
-    '''Creates a list of lists representing Pascal's triangle.
-
-    Args:
-        n (int): The number of rows to generate in the Pascal's triangle.
-
-    Returns:
-        list: A list of lists representing Pascal's triangle.
-
-    Raises:
-        TypeError: If the input `n` is not an integer.
-        ValueError: If the input `n` is less than or equal to 0.
+    '''Creates the list of lists of integers representing
+    the Pascal's triangle of a given integer.
     '''
     triangle = []
-
-    try:
-        if type(n) is not int or n <= 0:
-            raise ValueError("Input must be a positive integer.")
-
-        for i in range(n):
-            line = []
-            for j in range(i + 1):
-                if j == 0 or j == i:
-                    line.append(1)
-                elif i > 0 and j > 0:
-                    line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-            triangle.append(line)
-
-    except (TypeError, ValueError) as e:
-        # Print error message or handle the exception as needed
-        print(f"Error: {str(e)}")
+    if type(n) is not int or n <= 0:
         return triangle
-
+    for p in range(n):
+        line = []
+        for j in range(p + 1):
+            if j == 0 or j == p:
+                line.append(1)
+            elif p > 0 and j > 0:
+                line.append(triangle[p - 1][j - 1] + triangle[p - 1][j])
+        triangle.append(line)
     return triangle
