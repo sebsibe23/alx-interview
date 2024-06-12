571afbda54cb7413c0b8fd79af788868a8635400
sebsibe23 <sebsibe1990@proton.me>
2024-06-12 09:57:21 -0700

    fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -33,29 +33,24 @@ def rotate_2d_matrix(matrix):
     place by rotating it 90 degrees
     clockwise.
     """
-    try:
-        if type(matrix) != list:
-            return
-        if len(matrix) <= 0:
-            return
-        if not all(map(lambda x: type(x) == list, matrix)):
-            return
-
-        rows = len(matrix)
-        cols = len(matrix[0])
-        if not all(map(lambda x: len(x) == cols, matrix)):
-            return
-
-        c, r = 0, rows - 1
-        for i in range(cols * rows):
-            if i % rows == 0:
-                matrix.append([])
-            if r == -1:
-                r = rows - 1
-                c += 1
-            matrix[-1].append(matrix[r][c])
-            if c == cols - 1 and r >= -1:
-                matrix.pop(r)
-            r -= 1
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
