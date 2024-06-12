fb334b52ca4b73ec7c0899ce45d0e648b55c1fe3
sebsibe23 <sebsibe1990@proton.me>
2024-06-12 10:02:37 -0700

    final fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,48 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
+"""
+2D matrix rotation module.
+
+This module provides a function to rotate
+an m byn 2D matrix in place by 90
+degrees clockwise.
+
+Function:
+    rotate_2d_matrix(matrix): Rotates the given
+    2D matrix in place.
+
+Parameters:
+    matrix (list of list of int): The 2D matrix
+    to be rotated. Each inner list
+    represents a row in the matrix.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    Rotates an m by n 2D matrix in place.
+
+    Args:
+        matrix (list of list of int): The 2D matrix
+        to be rotated. Each inner
+        list represents a row in the matrix.
+
+    Returns:
+        None
+
+    This function modifies the input matrix in
+    place by rotating it 90 degrees
+    clockwise.
+    """
+    try:
+        n = len(matrix)
+
+    # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse each row
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
+    except Exception as ex:
+        print(ex)
